6dec6ef4f568d5f6d54d9bef1047b601c658355a
boostex1 <mirkamolmirazizov5@gmail.com>
2025-02-11 22:48:26 +0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,19 +1,15 @@
 from hikka import loader, utils  
-import asyncio  
 
 @loader.tds
 class HelloMod(loader.Module):
     """Простой модуль для Hikka, который отвечает 'Привет, бро!'"""
 
-    strings = {"name": "HelloMod"} 
+    strings = {"name": "HelloModBoost"}  
 
     @loader.command()
     async def hello(self, message):
-        """Отвечает 'Привет, бро!' и самоуничтожается"""
+        """Отвечает 'Привет, бро!'"""
         args = utils.get_args_raw(message)  
         text = f"Привет, {args}!" if args else "Привет, бро!"  
-
-        reply = await utils.answer(message, text)  
-        await message.delete()  
-        await asyncio.sleep(5)  
-        await reply.delete()  
+        
+        await utils.answer(message, text)  
